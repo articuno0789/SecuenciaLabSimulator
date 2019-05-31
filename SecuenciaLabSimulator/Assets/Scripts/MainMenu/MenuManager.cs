0cf9294175fa31792d0eea1f690e36635360acad
Cristian Michell Castillo Serrano <agua_cristian@hotmail.com>
2019-05-31 00:12:42 -0500

    Menu principal y movimiento dentro de la escena principal
    He creado un menu principal con diferentes opciones personalizadas y he creado una forma de mover la camara y desplazarnos por el terreno para interactuar con la maquina.

---
+++ SecuenciaLabSimulator/Assets/Scripts/MainMenu/MenuManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void LoadByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
+    }
+
+    public EventSystem eventSystem;
+    public GameObject selectedObject;
+
+    private bool buttonSelected;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Vertical") != 0 && buttonSelected == false)
+        {
+            eventSystem.SetSelectedGameObject(selectedObject);
+            buttonSelected = true;
+        }
+    }
+
+    private void OnDisable()
+    {
+        buttonSelected = false;
+    }
+}
