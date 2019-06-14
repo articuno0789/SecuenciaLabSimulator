a04aafb267e969351b0909bff6d35f0216ead091
Cristian Michell Castillo Serrano <agua_cristian@hotmail.com>
2019-06-13 23:04:35 -0500

    Animaciones de presioanr botones
    En este commit se agregpan los botones de algunos modulos para que siempre esten a las mismas distancias.
    Asimsimo se crean las animaciones de presionar botones apra esos modulos.

---
+++ SecuenciaLabSimulator/Assets/Scripts/Modulos/Modulo2/Mod2PushButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mod2PushButton : MonoBehaviour
+{
+    private Animation ani;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ani = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (name.Contains("BotonCuadradoRojo")) {
+            Debug.Log("Entra a presionar boton rojo cuadrado");
+            ani.Play("Mod2PresBotonCuadradoRojo");
+        }else if (name.Contains("BotonCuadradoVerde"))
+        {
+            Debug.Log("Entra a presionar boton verde cuadrado");
+            ani.Play("Mod2PresBotonCuadradoVerde");
+        }
+    }
+}
---
+++ SecuenciaLabSimulator/Assets/Scripts/Modulos/Modulo3/Mod3PushButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mod3PushButton : MonoBehaviour
+{
+    private Animation ani;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ani = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Entra a presionar boton");
+        ani.Play("Mod3PresBotonCircular");
+    }
+}
