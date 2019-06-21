1279d421a53512086436fc8033f6bf7297887709
Cristian Michell Castillo Serrano <agua_cristian@hotmail.com>
2019-06-20 20:43:44 -0500

    Creación de un prototipo de sistema para la generación dinámica de los cables de conexión
    En este commit se creó el prototipo del sistema para la generación de cables y conexiones entre los plugs del sistema.
    Este sistema funciona medianamente bien, pero lo deshabilite por el momento para mejorarlos.
    Para crear este sistema se debieron utilizar cálculo de integrales y curvas de bezier.
    Asimismo, se alteró el archivo ClickDetector para agregar esta funcionalidad.

---
+++ SecuenciaLabSimulator/Assets/Scripts/CableComponent/Scripts/MouseDrag.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseDrag : MonoBehaviour
+{
+
+    float distance = 10;
+
+    private void Start()
+    {
+        GameObject cam = GameObject.Find("Main Camera");
+        float distance2 = Vector3.Distance(transform.position, cam.transform.position);
+        distance = distance2;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+        Vector3 objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        transform.position = objectPos;
+    }
+}
--- SecuenciaLabSimulator/Assets/Scripts/Player/ClickDetector.cs
+++ SecuenciaLabSimulator/Assets/Scripts/Player/ClickDetector.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 using System.Collections;
 public class ClickDetector : MonoBehaviour
 {
-    public bool HandleLeftClick = true;
+    public Material cableMaterial;
+    public bool HandleLeftClick = false;
     public bool HandleRightClick = true;
     public bool HandleMiddleClick = false;
     public string OnLeftClickMethodName = "Prueba";
@@ -17,16 +18,123 @@ public class ClickDetector : MonoBehaviour
         GameObject clickedGmObj = null;
         bool clickedGmObjAcquired = false;
         // Left click
-        if (HandleLeftClick && Input.GetMouseButtonDown(0))
+        /*if (HandleLeftClick && Input.GetMouseButtonDown(0))
         {
-            /*if (!clickedGmObjAcquired)
-            {*/
-            clickedGmObj = GetClickedGameObject();
-            clickedGmObjAcquired = true;
-            /*}*/
+            if (!clickedGmObjAcquired)
+            {
+                clickedGmObj = GetClickedGameObject();
+                //lastClickedGmObj = clickedGmObj;
+                clickedGmObjAcquired = true;
+            }
             if (clickedGmObj != null)
-                clickedGmObj.SendMessage(OnLeftClickMethodName, null, SendMessageOptions.DontRequireReceiver);
-        }
+            {
+                bool segundoPlug = false;
+                Debug.Log("Manda mensaje, click izquierdo: " + OnLeftClickMethodName + ", *******Objeto clic: " + clickedGmObj.name);
+                if(lastClickedGmObj != null) {
+                    Debug.Log("lastClickedGmObj != null");
+                    if (!lastClickedGmObj.name.Contains("EntradaPlug"))
+                    {
+                        Debug.Log("Entra !lastClickedGmObj.name.Contains(\"EntradaPlug\")");
+                        if (clickedGmObj.name.Contains("EntradaPlug"))
+                        {
+                            Debug.Log("NO HAY CAMBIO DE COLOR");
+                            //Fetch the Renderer from the GameObject
+                            Renderer rend = clickedGmObj.GetComponent<Renderer>();
+                            //Set the main Color of the Material to green
+                            //rend.material.shader = Shader.Find("_Color");
+                            //rend.material.SetColor("_Color", Color.green);
+                            rend.material.color = Color.green;
+                        }
+                        else
+                        {
+                            Renderer rend = clickedGmObj.GetComponent<Renderer>();
+                            rend.material.color = Color.yellow;
+                            Debug.Log("-------CAMBIO DE COLOR");
+                            //Fetch the Renderer from the GameObject
+                            //Renderer rend = clickedGmObj.GetComponent<Renderer>();
+                            //Set the main Color of the Material to green
+                            //rend.material.shader = Shader.Find("_Color");
+                            //rend.material.SetColor("_Color", Color.green);
+                        }
+                    }
+                    else
+                    {
+                        if (clickedGmObj.name.Contains("EntradaPlug") && clickedGmObj != lastClickedGmObj)
+                        {
+                            Renderer rend = clickedGmObj.GetComponent<Renderer>();
+                            rend.material.color = Color.red;
+                            segundoPlug = true;
+
+                            CableComponent cableCompStart = lastClickedGmObj.GetComponent<CableComponent>();
+                            CableComponent cableCompEnd = clickedGmObj.GetComponent<CableComponent>();
+
+                            if (cableCompStart.endPoint != null)
+                            {
+                                CableComponent cableCompLastEndPointStart = cableCompStart.endPoint.GetComponent<CableComponent>();
+                                GameObject endPoint = cableCompStart.endPoint;
+                                Debug.Log("GameObject endPoint = cableCompStart.endPoint;");
+                                Destroy(cableCompLastEndPointStart);
+                                Debug.Log("Destroy(cableCompLastEndPointStart);");
+                                Destroy(cableCompStart);
+                                Debug.Log("Destroy(cableCompStart);");
+                                //cableCompLastEndPointStart.endPoint = null;
+                                //cableCompLastEndPointStart.startPoint = null;
+                                Destroy(lastClickedGmObj.GetComponent<LineRenderer>());
+                                Destroy(cableCompStart.endPoint.GetComponent<LineRenderer>());
+
+                                endPoint.AddComponent(typeof(CableComponent));
+                                Debug.Log("endPoint.AddComponent(typeof(CableComponent));");
+                                cableCompStart = lastClickedGmObj.AddComponent(typeof(CableComponent)) as CableComponent;
+                                Debug.Log("lastClickedGmObj.AddComponent(typeof(CableComponent)) as CableComponent;");
+                            }
+                            //CableComponent cableCompEnd2 = cableCompEnd;
+                           // GameObject endEndPoint = cableCompEnd2.endPoint;
+                            //    if (cableCompEnd2.endPoint != null)
+                            //    {
+                            //        CableComponent cableCompLastEndPointEnd = endEndPoint.GetComponent<CableComponent>();
+                            //        Destroy(cableCompLastEndPointEnd);
+                            //        Destroy(clickedGmObj.GetComponent<CableComponent>());
+                           //         Destroy(clickedGmObj.GetComponent<LineRenderer>());
+                           //         Destroy(endEndPoint.GetComponent<LineRenderer>());
+
+                           //         endEndPoint.AddComponent(typeof(CableComponent));
+                           //         cableCompEnd = clickedGmObj.AddComponent(typeof(CableComponent)) as CableComponent;
+                           //     Debug.Log("**********************************************cableCompEnd2.endPoint != null");
+                           // }
+
+
+                            cableCompStart.endPoint = clickedGmObj;
+                            cableCompStart.startPoint = lastClickedGmObj;
+
+                            //cableCompStart.cableMaterial = (Material)Resources.Load("CableMaterial.mat", typeof(Material));
+                            cableCompStart.cableMaterial = cableMaterial;
+                            cableCompStart.InitCableParticles();
+                            cableCompStart.InitLineRenderer();
+
+                            
+
+                            cableCompEnd.endPoint = lastClickedGmObj;
+                            cableCompEnd.startPoint = clickedGmObj;
+                            cableCompEnd.cableMaterial = cableMaterial;
+                            lastClickedGmObj.GetComponent<Renderer>().material.color = Color.white;
+                            clickedGmObj.GetComponent<Renderer>().material.color = Color.white;
+                        }
+                        else
+                        {
+                            Renderer rend = lastClickedGmObj.GetComponent<Renderer>();
+                            rend.material.color = Color.white;
+                            segundoPlug = true;
+                        }
+                    }
+                }
+                lastClickedGmObj = clickedGmObj;
+                if (segundoPlug)
+                {
+                    lastClickedGmObj = GameObject.Find("Plane");
+                }
+                //clickedGmObj.SendMessage(OnLeftClickMethodName, null, SendMessageOptions.DontRequireReceiver);
+            }
+        }*/
         // Right click
         if (HandleRightClick && Input.GetMouseButtonDown(1))
         {
@@ -43,8 +151,8 @@ public class ClickDetector : MonoBehaviour
                 clickedGmObj.SendMessage("OpenCloseMenuChangeModule", 1, SendMessageOptions.DontRequireReceiver);
                 //clickedGmObj.SendMessage("Prueba", 1);
             }
-
-            //clickedGmObj.SendMessage(OnRightClickMethodName, null, SendMessageOptions.DontRequireReceiver);
+                
+                //clickedGmObj.SendMessage(OnRightClickMethodName, null, SendMessageOptions.DontRequireReceiver);
         }
         // Middle click
         if (HandleMiddleClick && Input.GetMouseButtonDown(2))
@@ -73,7 +181,7 @@ public class ClickDetector : MonoBehaviour
         {
             Debug.Log("eNTRA A NULO, x: " + Input.mousePosition.x + ", y: " + Input.mousePosition.y + ", z: " + Input.mousePosition.z);
             return null;
-        }
+        }   
     }
 
 
