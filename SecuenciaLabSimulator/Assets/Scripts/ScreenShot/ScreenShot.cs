e3855780e25ad40e8ff973e1cca35741bee292d4
Cristian Michell Castillo Serrano <agua_cristian@hotmail.com>
2019-06-26 20:15:06 -0500

    Funcionalidad para guardar screenshot dentro del simulador.
    En este commit se creo la funcionalidad de tomar capturas de pantalla dentro del simulador. Estas capturas de pantalla se almacenan en una carpeta determinada fuera del proyecto.
    Estas capturas se toman de acuerdo a la dirección de la cámara del jugador.
    La tecla para activar la función es f10.

---
+++ SecuenciaLabSimulator/Assets/Scripts/ScreenShot/ScreenShot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShot : MonoBehaviour
+{
+    GameObject camaraSound;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+        camaraSound = GameObject.Find("CamaraEffect");
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F10))
+        {
+            string screenshotIMGName = System.DateTime.Now.ToString();
+            string subString = screenshotIMGName.Replace('/', '_');
+            string gypsy = subString.Replace(':', '_');
+            Debug.Log("Screen shot captured: " + gypsy + ".png");
+            ScreenCapture.CaptureScreenshot(gypsy + ".png");
+            if(camaraSound != null)
+            {
+                AudioSource sound = camaraSound.GetComponent<AudioSource>();
+                if(sound != null)
+                {
+                    Debug.Log("Suena sonido camara");
+                    sound.Play();
+                }
+            }
+        }
+    }
+}
