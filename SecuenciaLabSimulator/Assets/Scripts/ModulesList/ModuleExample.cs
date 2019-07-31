b63a27e32b311d595f5f9a26b5f4bc521dfa7be6
Cristian Michell Castillo Serrano <agua_cristian@hotmail.com>
2019-07-30 23:57:27 -0500

    Creación de la lista de módulos actuales simulador
    En este commit se creó la lista de módulos actuales del simulador. Esta lista mantiene una referencia (entro otros elementos) de los módulos que tienen el simulador en el momento actual, este elemento auxiliar sirve para:
    Permitir realizar las restricciones de creación de módulos.
    Permitir la carga y guardado de módulos.
    Permitir saber en todo momento los módulos actuales del simulador.

---
+++ SecuenciaLabSimulator/Assets/Scripts/ModulesList/ModuleExample.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ModuleExample
+{
+    public string title;
+    public string nameModule;
+    [TextArea]
+    public string description;
+    public GameObject modelSystemGO;
+    public Vector3 modelPosition, modelRotation, modelScale;
+
+    public ModuleExample()
+    {
+
+    }
+
+    public ModuleExample(string title, string nameModule, string description, GameObject modelSystemGO,
+        Vector3 modelPosition, Vector3 modelRotation, Vector3 modelScale)
+    {
+        this.title = title;
+        this.nameModule = nameModule;
+        this.description = description;
+        this.modelSystemGO = modelSystemGO;
+        this.modelPosition = modelPosition;
+        this.modelRotation = modelRotation;
+        this.modelScale = modelScale;
+    }
+}
---
+++ SecuenciaLabSimulator/Assets/Scripts/ModulesList/ModulesList.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModulesList : MonoBehaviour
+{
+    [SerializeField] public ModuleExample[] modulesGroup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
