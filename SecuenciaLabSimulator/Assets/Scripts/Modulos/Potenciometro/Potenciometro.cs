040b7c6e3c2f02971f7dd5a20edb63add41b95b8
Cristian Michell Castillo Serrano <agua_cristian@hotmail.com>
2019-09-01 22:32:09 -0500

    Programación del comportamiento modulo Potenciometro
    En este commit se realizó de manera general la programación del comportamiento de este módulo, todavía falta optimizar su comportamiento y realizar algunas validaciones adicionales.

--- SecuenciaLabSimulator/Assets/Scripts/Modulos/Potenciometro/Potenciometro.cs
+++ SecuenciaLabSimulator/Assets/Scripts/Modulos/Potenciometro/Potenciometro.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Potenciometro : MonoBehaviour
 {
     #region Atributos
+    public bool moduloEncendido = true;
     public Dictionary<string, string> plugsConnections;
     [SerializeField] public List<GameObject> plugAnaranjados;
     [SerializeField] public List<GameObject> plugNegros;
@@ -17,7 +18,10 @@ public class Potenciometro : MonoBehaviour
     public float valorMinimoPerilla = 0.0f;
     public float valorMaximoPerilla = 100.0f;
     private Quaternion originalRotationKnob;
-
+    public bool potenciometroAveriado = false;
+    private ParticlesError particleError;
+    public int currentTypeParticleError = 7;
+    public GameObject currentParticle;
 
     [SerializeField] public float gradosActualesPerilla = 0.0f;
     [SerializeField] public float velocidadRotacion = 10;
@@ -29,8 +33,28 @@ public class Potenciometro : MonoBehaviour
     public bool mostrarDiccionarioConexiones = false; // Variable
     public bool mostrarPlugAnaranjados = false; // Variable
     public bool mostrarPlugNegros = false; // Variable
-
+    public bool debugMode = false;
     public bool Puederotar => puederotar;
+    #endregion
+
+    #region Propiedades
+    public bool DebugMode
+    {
+        get => debugMode;
+        set => debugMode = value;
+    }
+
+    public int CurrentTypeParticleError
+    {
+        get => currentTypeParticleError;
+        set => currentTypeParticleError = value;
+    }
+
+    public bool PotenciometroArreglado
+    {
+        get => potenciometroAveriado;
+        set => potenciometroAveriado = value;
+    }
     #endregion
 
     #region Inicializacion
@@ -42,13 +66,15 @@ public class Potenciometro : MonoBehaviour
 
         plugAnaranjados = new List<GameObject>();
         plugNegros = new List<GameObject>();
+        particleError = new ParticlesError();
         InicializarComponentes(gameObject);
+        CurrentTypeParticleError = 7;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     private void InicializarComponentes(GameObject nodo)
@@ -94,6 +120,151 @@ public class Potenciometro : MonoBehaviour
     void Update()
     {
         ComprobarEstadosDiccionarios();
+        if (moduloEncendido)
+        {
+            //Hacer algo si el modulo esta encendido.
+            ComportamientoModulo();
+        }
+        else
+        {
+            //Hacer algo si el modulo esta apagado.
+        }
+    }
+
+    void ComportamientoModulo()
+    {
+        Plugs plugIzquierdoCompPlug = plugAnaranjadosDict["EntradaPlugAnaranjado1"].GetComponent<Plugs>();
+        Plugs plugDerechoCompPlug = plugAnaranjadosDict["EntradaPlugAnaranjado3"].GetComponent<Plugs>();
+
+        if (plugIzquierdoCompPlug != null && plugDerechoCompPlug != null)
+        {
+            plugIzquierdoCompPlug.EstablecerPropiedadesConexionesEntrantes();
+            plugDerechoCompPlug.EstablecerPropiedadesConexionesEntrantes();
+
+            if (plugIzquierdoCompPlug.Conectado && plugDerechoCompPlug.Conectado)
+            {
+                Plugs plugCentral = plugAnaranjadosDict["EntradaPlugAnaranjado2"].GetComponent<Plugs>();
+                if (plugIzquierdoCompPlug.TipoConexion == 1 && plugDerechoCompPlug.TipoConexion == 2)// Correcto - Linea y neutro conectado en de manera correcta
+                {
+                    potenciometroAveriado = false;
+                    float nuevoVoltaje = plugIzquierdoCompPlug.Voltaje * (valorActualPerilla / 100);
+                    plugCentral.EstablecerPropiedadesConexionesEntrantes(plugAnaranjadosDict["EntradaPlugAnaranjado1"]);
+                    plugCentral.Voltaje = nuevoVoltaje;
+                    plugCentral.estoConectado();
+                    if (DebugMode)
+                    {
+                        Debug.Log(name + ") " + this.name + " - POTENCIOMETRO - if(plugIzquierdoCompPlug.TipoConexion == 1 && plugDerechoCompPlug.TipoConexion == 2) - Conectado");
+                    }
+                }
+                else if (plugIzquierdoCompPlug.TipoConexion == 2 && plugDerechoCompPlug.TipoConexion == 1) //Averia - Linea y neutro invertido
+                {
+                    potenciometroAveriado = false;
+                    float nuevoVoltaje = plugDerechoCompPlug.Voltaje * (valorActualPerilla / 100);
+                    plugCentral.EstablecerPropiedadesConexionesEntrantes(plugAnaranjadosDict["EntradaPlugAnaranjado3"]);
+                    plugCentral.Voltaje = nuevoVoltaje;
+                    plugCentral.estoConectado();
+                    if (DebugMode)
+                    {
+                        Debug.Log(name + ") " + this.name + " - POTENCIOMETRO - (plugArribaCompPlug.TipoConexion == 2 && plugAbajoCompPlug.TipoConexion == 1) - Conectado -");
+                    }
+                }
+                else if (plugIzquierdoCompPlug.TipoConexion == 1 && plugDerechoCompPlug.TipoConexion == 1) // Avaeria - Dos lineas conectadas al mismo tiempo
+                {
+                    if (plugIzquierdoCompPlug.Linea == plugDerechoCompPlug.Linea)
+                    {
+                        potenciometroAveriado = false;
+                        plugCentral.EstablecerValoresNoConexion2();
+                        if (DebugMode)
+                        {
+                            Debug.Log(name + ") " + this.name + " - POTENCIOMETRO - (plugArribaCompPlug.Linea == plugAbajoCompPlug.Linea) - Conectado - Debido a que son la misma linea simplemente no enciende.");
+                        }
+                    }
+                    else if (plugIzquierdoCompPlug.Linea != plugDerechoCompPlug.Linea)
+                    {
+                        potenciometroAveriado = true;
+                        if (DebugMode)
+                        {
+                            Debug.LogError(name + ") " + this.name + " - POTENCIOMETRO - if (plugArribaCompPlug.Linea != plugAbajoCompPlug.Linea) - Conectado - Debido a que son lineas diferentes el foco se quema.");
+                        }
+                    }
+                    else
+                    {
+                        potenciometroAveriado = true;
+                        EliminarMaterial();
+                        if (DebugMode)
+                        {
+                            Debug.LogError(name + ") " + this.name + " - POTENCIOMETRO - NO DEBERIA ENTRAR AQUI - (plugArribaCompPlug.TipoConexion == 1 && plugAbajoCompPlug.TipoConexion == 1)");
+                        }
+                    }
+                }
+                else if (plugIzquierdoCompPlug.TipoConexion == 2 && plugDerechoCompPlug.TipoConexion == 2) // Correcto - Dos neutros conectados, no pasa nada
+                {
+                    potenciometroAveriado = false;
+                    plugCentral.EstablecerPropiedadesConexionesEntrantes(plugAnaranjadosDict["EntradaPlugAnaranjado3"]);
+                    if (DebugMode)
+                    {
+                        Debug.Log(name + ") " + this.name + " - POTENCIOMETRO - (plugIzquierdoCompPlug.TipoConexion == 2 && plugDerechoCompPlug.TipoConexion == 2) - Conectado");
+                    }
+                }
+                else
+                {
+                    potenciometroAveriado = true;
+                    EliminarMaterial();
+                    Debug.LogError(name + ") " + this.name + " - POTENCIOMETRO - Este caso de uso todavia no esta programado - No entro a ningun caso");
+                }
+            }
+            else
+            {
+                potenciometroAveriado = false;
+                //ApagarFoco();
+                if (DebugMode)
+                {
+                    Debug.Log(name + ") " + this.name + " - POTENCIOMETRO - if (plugIzquierdoCompPlug.Conectado && plugDerechoCompPlug.Conectado) - NO esta conectados");
+                }
+            }
+            ComprobarEstadoAveria();
+        }
+        else
+        {
+            //Debug.LogError(name + ") " + this.name + " - FOCO AMARILLO - if(plugArribaCompPlug != null && plugAbajoCompPlug != null) - Alguno de los dos es nulo, plugArribaCompPlug: " + plugIzquierdoCompPlug + ", plugAbajoCompPlug: " + plugDerechoCompPlug);
+        }
+    }
+
+    void ComprobarEstadoAveria()
+    {
+        if (potenciometroAveriado)
+        {
+            if (currentParticle == null)
+            {
+                CrearAveria();
+            }
+        }
+        else
+        {
+            if (currentParticle != null)
+            {
+                QuitarAveria();
+            }
+        }
+    }
+
+    public void CrearAveria()
+    {
+        currentParticle = particleError.CrearParticulasError(currentTypeParticleError, perilla.transform.position, perilla.transform.rotation.eulerAngles, new Vector3(0.5f, 0.5f, 0.5f));
+        currentParticle.transform.parent = this.gameObject.transform;
+        potenciometroAveriado = true;
+    }
+
+    public void QuitarAveria()
+    {
+        particleError.DestruirParticulasError(currentParticle);
+        potenciometroAveriado = false;
+    }
+
+    void EliminarMaterial()
+    {
+        Renderer perillaRender = perilla.GetComponent<Renderer>();
+        perillaRender.material = null;
     }
 
     public void RotarPerilla()
