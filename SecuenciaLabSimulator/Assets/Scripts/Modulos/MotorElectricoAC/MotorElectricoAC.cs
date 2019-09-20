37799db783dd97283b42cb0d2859245e41f11f3a
Cristian Michell Castillo Serrano <agua_cristian@hotmail.com>
2019-09-19 22:48:00 -0500

    Programacion logica funcionamiento motor
    En este commit s eincluyeron la programacion de la lógica del funcionameinto del motor y de los modulos 20 y 21.

--- SecuenciaLabSimulator/Assets/Scripts/Modulos/MotorElectricoAC/MotorElectricoAC.cs
+++ SecuenciaLabSimulator/Assets/Scripts/Modulos/MotorElectricoAC/MotorElectricoAC.cs
@@ -5,17 +5,98 @@ using UnityEngine;
 public class MotorElectricoAC : MonoBehaviour
 {
     #region Atributos
-    public bool moduloEncendido = true;
-    [SerializeField] public GameObject ejeMotor;
-    [SerializeField] public float velocidadRotacion = 10;
-    [SerializeField] public bool rotaMotorPrueba = true;
+    public bool motorEncendido = false;
+    public bool moduloExistente = false;
+    public GameObject moduloControlador;
+    public GameObject ejeMotor;
+    public GameObject cajaElectrica;
+    public float velocidadRotacionActual = 0;
+    public float velocidadMaximaRotacion = 1750;
+    public float velocidadMinimaRotacion = 0;
+    public int direccionRotacion = 0;
+    public float voltajeMaximo = 250; //250v
+    public float voltajeMinimo = 40; //0v
+    public float voltajeActual = 0; //velocidad actual
+    public bool rotaMotorPrueba = true;
+    public bool motorAveriado = false;
+    public GameObject panelInformativo;
+
+    public enum ParticlesErrorTypes
+    {
+        BigExplosion,
+        DrippingFlames,
+        ElectricalSparksEffect,
+        SmallExplosionEffect,
+        SmokeEffect,
+        SparksEffect,
+        RibbonSmoke,
+        PlasmaExplosionEffect
+    }
+    public GameObject currentParticle;
+    private ParticlesError particleError;
+    public int currentTypeParticleError = (int)ParticlesErrorTypes.SmokeEffect;
+    #endregion
+
+    #region Propiedades
+    public int DireccionRotacion
+    {
+        get => direccionRotacion;
+        set
+        {
+            if (value < 0 || value > 2)
+            {
+                throw new System.ArgumentOutOfRangeException(
+                      $"{nameof(value)} debe ser un valor entre 0 y 2. 0 sin rotacion, 1 - rotacion derecha, 2 - rotacion izquierda.");
+            }
+            direccionRotacion = value;
+        }
+    }
+
+    public float VelocidadRotacionActual
+    {
+        get => velocidadRotacionActual;
+        set
+        {
+            if (value < velocidadMinimaRotacion || value > velocidadMaximaRotacion)
+            {
+                throw new System.ArgumentOutOfRangeException(
+                      $"{nameof(value)}  valuedebe ser un valor entre: " + velocidadMinimaRotacion + " y " 
+                      + velocidadMaximaRotacion + ", El valor actual es: " + value);
+            }
+            velocidadRotacionActual = value;
+        }
+    }
+
+    public float VoltajeActual
+    {
+        get => voltajeActual;
+        set
+        {
+            if(value == 0)
+            {
+                voltajeActual = value;
+            }
+            else
+            if (value < voltajeMinimo || value > voltajeMaximo)
+            {
+                /*throw new System.ArgumentOutOfRangeException(
+                      $"{nameof(value)} debe ser un valor entre: " + voltajeMinimo + " y "
+                      + voltajeMaximo);*/
+                Debug.LogError($"{nameof(value)} debe ser un valor entre: " + voltajeMinimo + " y "
+                      + voltajeMaximo);
+            }
+            voltajeActual = value;
+        }
+    }
     #endregion
 
     #region Inicializacion
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
+        particleError = new ParticlesError();
+
         InicializarComponentes(gameObject);
+        ActualizarPanelInfo();
     }
 
     private void InicializarComponentes(GameObject nodo)
@@ -27,6 +108,9 @@ public class MotorElectricoAC : MonoBehaviour
             if (child.name.Contains("eje"))
             {
                 ejeMotor = child;
+            }else if (child.name.Contains("caja_electrica"))
+            {
+                cajaElectrica = child;
             }
             InicializarComponentes(child);
         }
@@ -37,12 +121,13 @@ public class MotorElectricoAC : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (moduloEncendido)
+        if (motorEncendido)
         {
             //Hacer algo si el modulo esta encendido.
             if (rotaMotorPrueba)
             {
-                ejeMotor.transform.Rotate(Vector3.right, -velocidadRotacion * Time.deltaTime);
+                RotarEjeMotor();
+                //ejeMotor.transform.Rotate(Vector3.right, -velocidadRotacionActual * Time.deltaTime);
             }
         }
         else
@@ -50,5 +135,145 @@ public class MotorElectricoAC : MonoBehaviour
             //Hacer algo si el modulo esta apagado.
         }
     }
+
+    public void RotarEjeMotor()
+    {
+        if (ejeMotor != null)
+        {
+            if(voltajeActual > voltajeMinimo && voltajeActual < voltajeMaximo)
+            {
+                float velo = velocidadRotacionActual/2;
+                if (direccionRotacion == 0) //Sin rotacion
+                {
+                    //Sin rotacion
+                }
+                else
+            if (direccionRotacion == 1) //Rotacion derecha - Sentido horario
+                {
+                    ejeMotor.transform.Rotate(Vector3.right, velo * Time.deltaTime);
+                }
+                else if (direccionRotacion == 2)// Rotacion Sentido antiorario
+                {
+                    ejeMotor.transform.Rotate(Vector3.left, velo * Time.deltaTime);
+                }
+                else
+                {
+                    Debug.LogError("Error." + name + ": Se establecio un tipo de rotacion del motor no permitida.");
+                }
+                QuitarAveria();
+            }
+            else
+            {
+                CrearAveria();
+            }
+            /*if (valorActualPerilla >= valorMinimoPerilla && valorActualPerilla <= valorMaximoPerilla)
+            {
+                //ejeMotor.transform.rotation = originalRotationKnob;
+                //float valorRotacionGrados = (limiteGiroSuperiorPerilla * valorActualPerilla) / valorMaximoPerilla;
+                //Debug.Log("Modulo 6: valorRotacionGrados: " + valorRotacionGrados + ", valorActualPerilla: " + valorActualPerilla + ", limiteGiroSuperiorPerilla: " + limiteGiroSuperiorPerilla + ", valorMaximoPerilla: " + valorMaximoPerilla);
+                ejeMotor.transform.Rotate(0, 0, velocidadRotacionActual);
+            }
+            else
+            {
+                Debug.LogError("Error." + name + " Modulo 6: rotarPerilla(float valorActual): El valor actual recibido sobrepasa los limites establecidos");
+            }*/
+        }
+    }
+
+    public void EncenderMotor()
+    {
+        motorEncendido = true;
+        ActualizarPanelInfo();
+    }
+
+    public void ApagarMotor()
+    {
+        motorEncendido = false;
+        ActualizarPanelInfo();
+    }
+
+    public void ReiniciarMotor()
+    {
+        ApagarMotor();
+        DireccionRotacion = 0;
+        VelocidadRotacionActual = 0;
+        VoltajeActual = 0;
+        ActualizarPanelInfo();
+    }
+
+    public void EstablecerParametrosMotor(bool encenderMotor, int directRotacion, float voltaje)
+    {
+        if (encenderMotor)
+        {
+            EncenderMotor();
+        }
+        else
+        {
+            ApagarMotor();
+        }
+        DireccionRotacion = directRotacion;
+        VelocidadRotacionActual = CalcularVelocidadDeReotacion();
+        VoltajeActual = voltaje;
+        ActualizarPanelInfo();
+    }
+
+    public float CalcularVelocidadDeReotacion()
+    {
+        float veloRotacion2;
+        if (VoltajeActual != 0)
+        {
+            veloRotacion2 = ((velocidadMaximaRotacion) / (voltajeMaximo - voltajeMinimo)) * (VoltajeActual - voltajeMinimo);
+        }
+        else
+        {
+            veloRotacion2 = 0;
+        }
+        return veloRotacion2;
+    }
+
+    public void ActualizarPanelInfo()
+    {
+        if(panelInformativo!= null)
+        {
+            panelInformativo.GetComponent<MotorStatePanel>().EstablecerTextoVoltajeMotor(voltajeActual);
+            panelInformativo.GetComponent<MotorStatePanel>().EstablecerTextoVeloMotor(velocidadRotacionActual);
+            panelInformativo.GetComponent<MotorStatePanel>().EstablecerTextoDireccionGiroMotor(direccionRotacion);
+            if (!motorAveriado)
+            {
+                panelInformativo.GetComponent<MotorStatePanel>().EstablecerTextoEstadoMotor(!motorAveriado);
+            }
+            else
+            {
+                panelInformativo.GetComponent<MotorStatePanel>().EstablecerTextoEstadoMotor(motorAveriado, "Descripcion de falla de pueba");
+            }
+        }
+        else
+        {
+
+        }
+    }
+
+    public void CrearAveria()
+    {
+        if (!motorAveriado)
+        {
+            currentParticle = particleError.CrearParticulasError(currentTypeParticleError, cajaElectrica.transform.position, 
+                cajaElectrica.transform.rotation.eulerAngles, new Vector3(5.5f, 5.5f, 5.5f));
+            currentParticle.transform.parent = this.gameObject.transform;
+            motorAveriado = true;
+        }
+        ActualizarPanelInfo();
+    }
+
+    public void QuitarAveria()
+    {
+        if (currentParticle != null)
+        {
+            particleError.DestruirParticulasError(currentParticle);
+            motorAveriado = false;
+        }
+        ActualizarPanelInfo();
+    }
+
     #endregion
 }
---
+++ SecuenciaLabSimulator/Assets/Scripts/Modulos/MotorElectricoAC/MotorStatePanel.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+
+public class MotorStatePanel : MonoBehaviour
+{
+    #region Atributos
+    public GameObject panel;
+    public Text estadoMotor;
+    public Text voltajeMotor;
+    public Text veloRotaMotor;
+    public Text direccionGiro;
+    //Debug
+    public bool debug = false;
+    #endregion
+
+    #region Inicializacion
+    // Use this for initialization
+    void Start()
+    {
+        /*if (player == null)
+        {
+            player = GameObject.Find("FirstPersonCharacter");
+        }
+        if (currentModuleSelected == null)
+        {
+            currentModuleSelected = GameObject.Find("CurrentModuleSelectedKnob").GetComponent<Text>();
+        }
+        if (minMaxKnobRange == null)
+        {
+            minMaxKnobRange = GameObject.Find("MinMaxValueKnob").GetComponent<Text>();
+        }
+        if (textInfoValueKnob == null)
+        {
+            textInfoValueKnob = GameObject.Find("TextInfoValueKnob").GetComponent<Text>();
+        }
+        if (panel == null)
+        {
+            panel = GameObject.Find("PanelSetValueKnob");
+        }
+        if (inputFieldCurrentValue == null)
+        {
+            inputFieldCurrentValue = GameObject.Find("InputFieldCurrentValue").GetComponent<InputField>();
+        }
+        if (buttonSetValueKnob == null)
+        {
+            buttonSetValueKnob = GameObject.Find("ButtonSetValueKnob").GetComponent<Button>();
+        }*/
+    }
+    #endregion
+
+    #region Comportamiento
+
+    public void EstablecerTextoVoltajeMotor(float voltaje)
+    {
+        if(voltajeMotor != null)
+        {
+            voltajeMotor.text = "Voltaje: " + voltaje + " v";
+        }
+    }
+
+    public void EstablecerTextoVeloMotor(float velocidad)
+    {
+        veloRotaMotor.text = "Velocidad de rotación: " + velocidad + " rpm";
+    }
+
+    public void EstablecerTextoEstadoMotor(bool todoBien, string textoExplicativo = "")
+    {
+        if (todoBien)
+        {
+            estadoMotor.text = "Estado: Ok";
+        }
+        else
+        {
+            estadoMotor.text = "Estado: Hay alguna falla. Descripción: " + textoExplicativo;
+        }
+    }
+
+    public void EstablecerTextoDireccionGiroMotor(int directGiro)
+    {
+        if (directGiro == 0)
+        {
+            direccionGiro.text = "Dirección giro: -";
+        }
+        else if(directGiro == 1)
+        {
+            direccionGiro.text = "Dirección giro: Horario";
+        }else if (directGiro == 2)
+        {
+            direccionGiro.text = "Dirección giro: Antihorario";
+        }
+        else
+        {
+            direccionGiro.text = "Dirección giro: --";
+        }
+    }
+
+    public void PararMotorTodoBien()
+    {
+        voltajeMotor.text = "Votaje: 0 v";
+        veloRotaMotor.text = "Velocidad de rotación: 0 rpm";
+        estadoMotor.text = "Estado: Ok";
+        direccionGiro.text = "Dirección giro: -";
+    }
+    #endregion
+}
