312bea2184dde2e9d20b1c9d847b92aca36980e3
Cristian Michell Castillo Serrano <agua_cristian@hotmail.com>
2019-09-26 20:54:35 -0500

    Creación de componentes auxiliares para componentes, animaciones y materiales
    En este commit se agregaron estrcuturas de apoyo para todos los compoenntes del simulador, para sustituir a algunas instrucciones que solo se podian utilizar en el editor.

---
+++ SecuenciaLabSimulator/Assets/Scripts/AnimationList/AnimationExample.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AnimationExample
+{
+    #region Atributos
+    [Header("Atributos Animación")]
+    public string title;
+    public string nameAnimation;
+    [TextArea]
+    public string description;
+    public AnimationClip animationSystemGO;
+    #endregion
+
+    #region Comportamiento
+    /*Contructor vacío.*/
+    public AnimationExample()
+    {
+
+    }
+
+    /*Contructor para llenar todos los parámetros.*/
+    public AnimationExample(string title, string nameAnimation, string description, AnimationClip animationSystemGO)
+    {
+        this.title = title;
+        this.nameAnimation = nameAnimation;
+        this.description = description;
+        this.animationSystemGO = animationSystemGO;
+    }
+    #endregion
+}
---
+++ SecuenciaLabSimulator/Assets/Scripts/AnimationList/AnimationList.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationList : MonoBehaviour
+{
+    #region Atributos
+    [Header("Lista de Animaciones")]
+    [SerializeField] public AnimationExample[] animationsGroup;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
