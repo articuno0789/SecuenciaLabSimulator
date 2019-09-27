e1755b84821e88e3ffab6a5f20f685609b40870e
Cristian Michell Castillo Serrano <agua_cristian@hotmail.com>
2019-09-26 20:56:59 -0500

    Estructura auxiliar de materiales, para sustituir isntrucciones exclusivas del simulador

---
+++ SecuenciaLabSimulator/Assets/Scripts/MaterialList/MaterialExample.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MaterialExample
+{
+    #region Atributos
+    [Header("Atributos Material")]
+    public string title;
+    public string nameMaterial;
+    [TextArea]
+    public string description;
+    public Material materialSystemGO;
+    #endregion
+
+    #region Comportamiento
+    /*Contructor vacío.*/
+    public MaterialExample()
+    {
+
+    }
+
+    /*Contructor para llenar todos los parámetros.*/
+    public MaterialExample(string title, string nameMaterial, string description, Material materialSystemGO)
+    {
+        this.title = title;
+        this.nameMaterial = nameMaterial;
+        this.description = description;
+        this.materialSystemGO = materialSystemGO;
+    }
+    #endregion
+}
+
