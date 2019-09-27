9159756600e985c00d79d826a843b8dd3cc1aba0
Cristian Michell Castillo Serrano <agua_cristian@hotmail.com>
2019-09-26 20:57:15 -0500

    Inclusión de header y validación de compoentes nulos

--- SecuenciaLabSimulator/Assets/Scripts/ScreenShot/ScreenShot.cs
+++ SecuenciaLabSimulator/Assets/Scripts/ScreenShot/ScreenShot.cs
@@ -18,14 +18,20 @@ public class ScreenShot : MonoBehaviour
     void Awake()
     {
         DontDestroyOnLoad(this);
-        if(camaraSound == null)
-        {
-            camaraSound = GameObject.Find("CamaraEffect");
-        }
+        ComprobarElementosExternos();
     }
     #endregion
 
     #region Comportamiento
+
+    void ComprobarElementosExternos()
+    {
+        if (camaraSound == null)
+        {
+            camaraSound = GameObject.Find("CamaraEffect");
+        }
+    }
+
     //En este método se comprueba las entradas por teclado.
     void Update()
     {
@@ -39,15 +45,15 @@ public class ScreenShot : MonoBehaviour
     //Se encarga de tomar la captura de pantalla de acuerdo a la visión de la camara principal.
     public void TomarCapturaDePantalla()
     {
+        ComprobarElementosExternos();
         /* Se obtiene la fecha del sistema y se le da el formato adecuado para estar en el nombre de un archivo.
          * Es decir, se eliminan los carácteres no válidos.*/
         string screenshotIMGName = System.DateTime.Now.ToString();
         string subString = screenshotIMGName.Replace('/', '_');
         string gypsy = subString.Replace(':', '_');
-
+        //Application.persistentDataPath
         //Se guarda la imagen en la ruta especificada.
         ScreenCapture.CaptureScreenshot(gypsy + ".png");
-
         //Se reproduce el sonido de camara.
         if (camaraSound != null)
         {
@@ -57,6 +63,10 @@ public class ScreenShot : MonoBehaviour
                 Debug.Log("Suena sonido camara");
                 sound.Play();
             }
+        }
+        else
+        {
+            Debug.LogError(this.name + ", Error. void TomarCapturaDePantalla() - El sonido de la camara no sono, hay un problema con ese objeto.");
         }
         if (debug)
         {
